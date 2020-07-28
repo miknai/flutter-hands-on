3ff9bab5945b38dca199e35e54f94bac4db1dd97
geonsoo.kim <gkim360@gatech.edu>
2020-07-27 23:07:58 -0500

    grid fun

---
+++ grid_fun/android/app/src/main/java/kim/geonsoo/grid_fun/MainActivity.java
@@ -0,0 +1,6 @@
+package kim.geonsoo.grid_fun;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ worldtime/android/app/src/main/java/kim/geonsoo/worldtime/MainActivity.java
@@ -0,0 +1,6 @@
+package kim.geonsoo.worldtime;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
